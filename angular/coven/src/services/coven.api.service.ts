73d9050b8ca6e5b48a198da18ccf854301a0c7d8
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-29 01:03:43 -0600

    type safe servicing

--- angular/coven/src/services/coven.api.service.ts
+++ angular/coven/src/services/coven.api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Author } from 'src/app/interfaces/Author';
+import { World } from 'src/app/interfaces/World';
 
 const endpoint = "https://localhost:7127/api/"; 
 
@@ -10,21 +11,20 @@ export class CovenApiService {
 
   constructor() { }
 
-  
-
   async GetWorldAnvilUser()
   {
     const response = await fetch(endpoint + "World/GetAnvilUser");
-    const data = await response.json();
-
+    const data = await response.json() as Author;
+  
     console.log(data);
+    return data;
   }
 
-  async GetWorldInfo()
-  {
+  async GetWorldInfo(): Promise<World> {
     const response = await fetch(endpoint + "World/GetWorldInfo");
-    const data = await response.json();
-
+    const data = await response.json() as World;
+  
     console.log(data);
+    return data;
   }
 }
