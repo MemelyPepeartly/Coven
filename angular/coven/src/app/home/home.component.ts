fd4b454fea71f03177d49a9108c6db797e578331
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-29 02:27:15 -0600

    functional home card

--- angular/coven/src/app/home/home.component.ts
+++ angular/coven/src/app/home/home.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CovenApiService } from 'src/services/coven.api.service';
+import { Author } from '../interfaces/Author';
+import { ImplicitReceiver } from '@angular/compiler';
+import { World, WorldsSummary } from '../interfaces/World';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(public covenService: CovenApiService) {}
 
+  author: Author | undefined;
+  summary: WorldsSummary | undefined;
+
+  async ngOnInit() {
+    
+  }
+
+  
+
   async getUser() {
-    await this.covenService.GetWorldAnvilUser();
+    this.author = await this.covenService.GetWorldAnvilUser();
   }
   async getWorld() {
-    await this.covenService.GetWorldInfo(); 
+    this.summary = await this.covenService.GetWorldInfo(); 
   }
 }
