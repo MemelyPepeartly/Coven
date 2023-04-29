5d8ed00da98dbf0116bcf7c4e56f3d79e08d4cfe
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-29 02:27:21 -0600

    interfacing

---
+++ angular/coven/src/app/interfaces/Article.ts
@@ -0,0 +1,60 @@
+import { Author } from "./Author";
+import { Category } from "./Category";
+import { WorldMeta } from "./World";
+
+export interface Article {
+    id: string;
+    title: string;
+    template: string;
+    is_wip: boolean;
+    is_draft: boolean;
+    state: string;
+    passcode: string;
+    wordcount: number;
+    creation_date: WorldAnvilDateDTO;
+    update_date: WorldAnvilDateDTO;
+    publication_date: WorldAnvilDateDTO;
+    notification_date: WorldAnvilDateDTO;
+    tags: string;
+    url: string;
+    category: Category; // Make sure to define the Category interface, based on your Category class
+    author: Author;
+    world: WorldMeta;
+    content: string;
+    content_parsed: string;
+    sections: any; // Replace with a more specific type if you know the structure
+    relations: any; // Replace with a more specific type if you know the structure
+    full_render: string;
+}
+export interface WorldAnvilDateDTO {
+    date: string; // Note: Date type in C# is serialized as a string in JSON
+    timezone_type: number;
+    timezone: string;
+}
+
+export interface WorldArticlesSummary {
+    world: WorldMeta;
+    term: string;
+    offset: string;
+    order_by: string;
+    trajectory: string;
+    articles: ArticleMeta[];
+}
+export interface ArticleMeta {
+    id: string;
+    title: string;
+    state: string;
+    is_wip: boolean;
+    is_draft: boolean;
+    template_type: string;
+    wordcount?: number;
+    views?: number;
+    likes?: number;
+    exerpt: string;
+    tags: string;
+    adult_content: boolean;
+    last_update: WorldAnvilDateDTO;
+    url: string;
+    author: Author;
+    world: WorldMeta;
+}
--- angular/coven/src/app/interfaces/World.ts
+++ angular/coven/src/app/interfaces/World.ts
@@ -14,3 +14,21 @@ export interface World {
     author: Author;
   }
   
+export interface WorldMeta {
+    id: string;
+    title: string;
+    slug: string;
+    url: string;
+  }
+  
+export interface UserWorldMeta {
+    id: string;
+    name: string;
+    state: string;
+  }
+export interface WorldsSummary {
+  id: string;
+  username: string;
+  worlds: UserWorldMeta[];
+}
+
