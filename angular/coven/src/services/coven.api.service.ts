53fa2d66ca822ebf636ed06d02a81f1e3028f59e
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-28 23:27:08 -0600

    test call

--- angular/coven/src/services/coven.api.service.ts
+++ angular/coven/src/services/coven.api.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import fetch from 'node-fetch';
+
+const endpoint = "https://localhost:7127/api/"; 
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,14 @@ import { Injectable } from '@angular/core';
 export class CovenApiService {
 
   constructor() { }
+
+  
+
+  async GetWorldAnvilUser()
+  {
+    const response = await fetch(endpoint + "World/GetAnvilUser");
+    const data = await response.json();
+
+    console.log(data);
+  }
 }
