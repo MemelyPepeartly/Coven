1713115c1184cfcf6f8170ce9ca279ee8e34ba56
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-03 17:25:56 -0600

    article parsing

--- angular/coven/src/app/article/article.component.ts
+++ angular/coven/src/app/article/article.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Article } from '../interfaces/Article';
 import { CovenApiService } from 'src/services/coven.api.service';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ArticleComponent implements OnInit {
   constructor(public covenService: CovenApiService){}
 
-  @Input() selectedArticle!: Article;
+  @Input() selectedArticle: Article;
+  @Input() articleLoaded: boolean = false;
 
   async ngOnInit() {
   }
