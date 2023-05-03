c4f7d9b6523f4e0369d3ec0c76a555d0f9d0444a
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-03 17:23:49 -0600

    updated types

--- api/Coven/Coven.Logic/Meta Objects/ArticleMeta.cs
+++ api/Coven/Coven.Logic/Meta Objects/ArticleMeta.cs
@@ -13,16 +13,16 @@ namespace Coven.Logic.Meta_Objects
         public Guid id { get; set; }
         public string title { get; set; }
         public string state { get; set; }
-        public bool is_wip { get; set; }
-        public bool is_draft { get; set; }
-        public string template_type { get; set; }
+        public bool isWip { get; set; }
+        public bool isDraft { get; set; }
+        public string templateType { get; set; }
         public int? wordcount { get; set; }
         public int? views { get; set; }
         public int? likes { get; set; }
         public string exerpt { get; set; }
         public string tags { get; set; }
-        public bool adult_content { get; set; }
-        public WorldAnvilDateDTO last_update { get; set; }
+        public bool adultContent { get; set; }
+        public WorldAnvilDateDTO lastUpdate { get; set; }
         public string url { get; set; }
         public Author author { get; set; }
         public WorldMeta world { get; set; }
