09a5191d3f43f82e8698ca3ece4852da43919e9b
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 15:02:41 -0600

    embedding DTO

---
+++ api/Coven/Coven.Logic/DTO/AI/EmbeddingsDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenAI_API;
+using OpenAI_API.Embedding;
+
+namespace Coven.Logic.DTO.AI
+{
+    public class EmbeddingsDTO
+    {
+        public EmbeddingsDTO() { 
+            queryEmbeddings = new List<Embedding>();
+            worldEmbeddings = new List<Embedding>();
+        }
+
+        public List<Embedding> queryEmbeddings { get; set; }
+        public List<Embedding> worldEmbeddings { get; set; }
+    }
+
+    public class Embedding
+    {
+        public string characterSet { get; set; }
+        public List<Data> vector { get; set; }
+    }
+}
