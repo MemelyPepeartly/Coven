ddb369c004d226528263a095ba3335a030829593
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 15:02:34 -0600

    new request model

---
+++ api/Coven/Coven.Logic/Request Models/get/GetEmbeddingsRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coven.Logic.Request_Models.get
+{
+    public class GetEmbeddingsRequestModel
+    {
+        public string userInput { get; set; }
+    }
+}
