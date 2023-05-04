963c87c1eae7bfdc85985c7bd917a4190d9f48e2
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 15:01:52 -0600

    new AI structure for embeddings

--- api/Coven/Coven.Api/Controllers/AIController.cs
+++ api/Coven/Coven.Api/Controllers/AIController.cs
@@ -1,4 +1,6 @@
-﻿using Coven.Logic.Request_Models.Get;
+﻿using Coven.Api.Services;
+using Coven.Logic.DTO.AI;
+using Coven.Logic.Request_Models.get;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OpenAI_API;
@@ -12,26 +14,53 @@ namespace Coven.Api.Controllers
         private readonly OpenAIAPI _openAIAPI;
         private readonly IConfiguration _config;
 
-        public AIController(IConfiguration config)
+        private readonly IWorldAnvilService WorldAnvilService;
+
+        public AIController(IConfiguration config, IWorldAnvilService _worldAnvilService)
         {
             _config = config;
             _openAIAPI = new OpenAIAPI(_config["OPENAI_API_KEY"]);
+
+            WorldAnvilService = _worldAnvilService;
         }
 
-        [HttpPost("ChatTest")]
-        public async Task<ActionResult> ChatTest([FromBody]GetAIResponseTestModel model)
+        [HttpPost("GetArticleEmbeddings")]
+        public async Task<ActionResult> GetArticleEmbeddings([FromBody]GetEmbeddingsRequestModel model)
         {
-            var chat = _openAIAPI.Chat.CreateConversation();
-            chat.AppendUserInput(model.userInput);
-
-            string response = await chat.GetResponseFromChatbot();
-
-
-            return Ok(new
+            EmbeddingsDTO DTO = new EmbeddingsDTO();
+            foreach (string word in model.userInput.Split(" "))
             {
-                userInput = model.userInput,
-                message = response
-            });
+                var thing = await _openAIAPI.Embeddings.CreateEmbeddingAsync(word);
+
+                DTO.queryEmbeddings.Add(new Embedding()
+                {
+                    characterSet = word,
+                    vector = (await _openAIAPI.Embeddings.CreateEmbeddingAsync(word)).Data
+                }) ;
+            }
+            
+
+            List<string> existingEmbeddings = (await WorldAnvilService.GetWorlds()).worlds
+                .Select(s => s.name)
+                .ToList();
+
+            foreach (string thing in existingEmbeddings)
+            {
+                DTO.queryEmbeddings.Add(new Embedding()
+                {
+                    characterSet = thing,
+                    vector = (await _openAIAPI.Embeddings.CreateEmbeddingAsync(thing)).Data
+                });
+            }
+
+            var result = CompareEmbeddings(DTO);
+
+            return Ok(result);
+        }
+
+        private object CompareEmbeddings(object DTO)
+        {
+            return true;
         }
     }
 }
