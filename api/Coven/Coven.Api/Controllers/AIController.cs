2a8c139e7ad54cebc26ed854d7aace38ea7b17f5
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 21:16:09 -0600

    new embeddings logic

--- api/Coven/Coven.Api/Controllers/AIController.cs
+++ api/Coven/Coven.Api/Controllers/AIController.cs
@@ -1,9 +1,12 @@
 ﻿using Coven.Api.Services;
+using Coven.Data.Repository;
 using Coven.Logic.DTO.AI;
+using Coven.Logic.DTO.WorldAnvil;
 using Coven.Logic.Request_Models.get;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OpenAI_API;
+using OpenAI_API.Embedding;
 using System.Diagnostics;
 
 namespace Coven.Api.Controllers
@@ -15,13 +18,15 @@ namespace Coven.Api.Controllers
         private readonly OpenAIAPI _openAIAPI;
         private readonly IConfiguration _config;
 
+        private readonly IRepository Repository;
         private readonly IWorldAnvilService WorldAnvilService;
 
-        public AIController(IConfiguration config, IWorldAnvilService _worldAnvilService)
+        public AIController(IConfiguration config, IWorldAnvilService _worldAnvilService, IRepository _repository)
         {
             _config = config;
             _openAIAPI = new OpenAIAPI(_config["OPENAI_API_KEY"]);
 
+            Repository = _repository;
             WorldAnvilService = _worldAnvilService;
         }
 
@@ -63,6 +68,26 @@ namespace Coven.Api.Controllers
 
             return Ok(result);
         }
+
+        [HttpPost("AddEmbeddings")]
+        public async Task<ActionResult> AddEmbeddings(Guid userId, Guid worldId)
+        {
+            WorldsSummary worlds = await WorldAnvilService.GetWorlds();
+            List<string> articleTitles = (await WorldAnvilService.GetWorldArticlesSummary(worldId)).articles
+                .Select(s => s.title)
+                .ToList();
+            foreach (string title in articleTitles)
+            {
+                EmbeddingResult embedding = await _openAIAPI.Embeddings.CreateEmbeddingAsync(title);
+
+                var vectors = embedding.Data
+                    .SelectMany(t => t.Embedding)
+                    .ToArray();
+                await Repository.CreateEmbeddings(userId, title, vectors);
+            }
+            return Ok(await Repository.GetUserEmbeddings(userId));
+        }
+
 
         // Calculate the dot product of two vectors
         private static float DotProduct(float[] vecA, float[] vecB)
