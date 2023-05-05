f4e3fefdcbf7d7c0fa8a8a668f92767d3b0181f5
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 21:48:59 -0600

    moved implimentation

--- api/Coven/Coven.Api/Controllers/UserController.cs
+++ api/Coven/Coven.Api/Controllers/UserController.cs
@@ -1,7 +1,6 @@
 ﻿using Coven.Api.Services;
 using Coven.Data.Entities;
 using Coven.Data.Repository;
-using Coven.Logic.DTO.AI;
 using Coven.Logic.Request_Models.Post;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,33 +22,20 @@ namespace Coven.Api.Controllers
         [HttpGet("GetAllUsers")]
         public async Task<ActionResult> GetAllUsers()
         {
-            return Ok((await Repository.GetUsers())
-                .Select(u => new UserDTO()
-                {
-                    UserId = u.UserId,
-                    Username = u.Username,
-                    WorldAnvilUsername = u.WorldAnvilUsername,
-                    Email = u.Email,
-                    WacharacterSets = u.WacharacterSets.Select(cs => new WACharacterSetDTO()
-                    {
-                        CharacterSet = cs.CharacterSet,
-                        vectors = ByteArrayToFloatArray(cs.Waembeddings.SelectMany(v => v.Vector).ToArray())
-                    }).ToList()
-                })
-                .ToList());
+            return Ok((await Repository.GetDTOUsers()));
         }
 
         [HttpPost("CreateUser")]
-        public async Task<ActionResult> CreateUser([FromBody]CreateUserRequestModel model)
+        public async Task<ActionResult> CreateUser([FromBody] CreateUserRequestModel model)
         {
             return Ok(await Repository.CreateUser(model.username, model.worldAnvilUsername, model.email));
         }
 
-        private static float[] ByteArrayToFloatArray(byte[] byteArray)
+        [HttpGet("{userId}/GetExistingUserEmbeddings")]
+        public async Task<ActionResult> GetExistingUserEmbeddings(Guid userId)
         {
-            float[] floatArray = new float[byteArray.Length / sizeof(float)];
-            Buffer.BlockCopy(byteArray, 0, floatArray, 0, byteArray.Length);
-            return floatArray;
+#warning This isn't working yet
+            return Ok(await Repository.GetUserEmbeddings(userId));
         }
     }
 }
