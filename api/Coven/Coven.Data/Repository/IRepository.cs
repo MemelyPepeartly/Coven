651d34e94582d86f957ad2efff717b1d5cfd23ce
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 18:23:16 -0600

    removal of old implimentation

--- api/Coven/Coven.Data/Repository/IRepository.cs
+++ api/Coven/Coven.Data/Repository/IRepository.cs
@@ -3,52 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Coven.Data.Entities;
 
 namespace Coven.Data.Repository
 {
     public interface IRepository
     {
         #region Create
-        /// <summary>
-        /// Creates a new client and returns the new client object from the database
-        /// </summary>
-        /// <param name="username"></param>
-        /// <param name="email"></param>
-        /// <returns></returns>
-        Task<Client> CreateClient(string username, string email);
-        /// <summary>
-        /// Creates a new character and returns the new character object from the database
-        /// </summary>
-        /// <param name="userId"></param>
-        /// <param name="characterName"></param>
-        /// <returns></returns>
-        Task<Character> CreateCharacter(Guid userId, string characterName);
         #endregion
 
         #region Read
-        /// <summary>
-        /// Gets a list of all client objects
-        /// </summary>
-        /// <returns></returns>
-        Task<List<Client>> GetClients();
-        /// <summary>
-        /// Returns a client by userId
-        /// </summary>
-        /// <param name="userId"></param>
-        /// <returns></returns>
-        Task<Client> GetClient(Guid userId);
-        /// <summary>
-        /// Returns a list of all characters in the database
-        /// </summary>
-        /// <returns></returns>
-        Task<List<Character>> GetAllCharacters();
-        /// <summary>
-        /// Gets a specific character by characterId
-        /// </summary>
-        /// <param name="characterId"></param>
-        /// <returns></returns>
-        Task<Character> GetCharacter(Guid characterId);
         #endregion
 
         #region Update
--- api/Coven/Coven.Data/Repository/Repository.cs
+++ api/Coven/Coven.Data/Repository/Repository.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Coven.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Coven.Data.Entities;
 
 namespace Coven.Data.Repository
 {
@@ -18,56 +18,9 @@ namespace Coven.Data.Repository
         }
 
         #region Create
-        public async Task<Client> CreateClient(string username, string email)
-        {
-            Guid newUserId = Guid.NewGuid();
-            await CovenContext.Clients.AddAsync(new Client()
-            {
-                UserId = newUserId,
-                Username = username,
-                Email = email
-            });
-            await CovenContext.SaveChangesAsync();
-
-            return await GetClient(newUserId);
-        }
-        public async Task<Character> CreateCharacter(Guid userId, string characterName)
-        {
-            Guid characterId = Guid.NewGuid();
-            await CovenContext.Characters.AddAsync(new Character()
-            {
-                UserId = userId,
-                CharacterId = characterId,
-                CharacterName = characterName
-            });
-            await CovenContext.SaveChangesAsync();
-
-            return await GetCharacter(characterId);
-        }
         #endregion
 
         #region Read
-        public async Task<List<Client>> GetClients()
-        {
-            return await CovenContext.Clients.AsNoTracking()
-                .ToListAsync();
-        }
-        public async Task<Client> GetClient(Guid userId)
-        {
-            return await CovenContext.Clients.AsNoTracking()
-                .FirstAsync(c => c.UserId == userId);
-        }
-        public async Task<List<Character>> GetAllCharacters()
-        {
-            return await CovenContext.Characters.AsNoTracking()
-                .ToListAsync();
-        }
-
-        public async Task<Character> GetCharacter(Guid characterId)
-        {
-            return await CovenContext.Characters.AsNoTracking()
-                .FirstAsync(c => c.CharacterId == characterId);
-        }
         #endregion
 
         #region Update
