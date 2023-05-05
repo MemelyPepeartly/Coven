e3f17c40e380ba25405ba90445fd683fd5970079
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 21:15:55 -0600

    repo implimentations

--- api/Coven/Coven.Data/Repository/IRepository.cs
+++ api/Coven/Coven.Data/Repository/IRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Coven.Data.Entities;
+using OpenAI_API.Embedding;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +11,14 @@ namespace Coven.Data.Repository
     public interface IRepository
     {
         #region Create
+        Task<User> CreateUser(string username, string worldAnvilUsername, string email);
+        Task<bool> CreateEmbeddings(Guid userId, string characterSet, float[] vectors);
         #endregion
 
         #region Read
+        Task<User> GetUser(Guid userId);
+        Task<List<User>> GetUsers();
+        Task<List<WacharacterSet>> GetUserEmbeddings(Guid userId);
         #endregion
 
         #region Update
@@ -19,5 +26,7 @@ namespace Coven.Data.Repository
 
         #region Delete
         #endregion
+
+        Task<bool> SaveAsync();
     }
 }
--- api/Coven/Coven.Data/Repository/Repository.cs
+++ api/Coven/Coven.Data/Repository/Repository.cs
@@ -1,6 +1,8 @@
 ﻿using Coven.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using OpenAI_API.Embedding;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,9 +20,108 @@ namespace Coven.Data.Repository
         }
 
         #region Create
+
+        public async Task<User> CreateUser(string username, string worldAnvilUsername, string email)
+        {
+            try
+            {
+                Guid userId = Guid.NewGuid();
+                await CovenContext.Users.AddAsync(new User()
+                {
+                    UserId = userId,
+                    Username = username,
+                    WorldAnvilUsername = worldAnvilUsername,
+                    Email = email
+                });
+
+                await SaveAsync();
+
+                return await CovenContext.Users.FirstAsync(x => x.UserId == userId);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> CreateEmbeddings(Guid userId, string characterSet, float[] vectors)
+        {
+            try
+            {
+                Guid characterId = Guid.NewGuid();
+
+                await CovenContext.WacharacterSets.AddAsync(new WacharacterSet()
+                {
+                    CharacterSetId = characterId,
+                    CharacterSet = characterSet,
+                    UserId = userId
+                });
+                await CovenContext.SaveChangesAsync();
+
+                byte[] byteArray = new byte[vectors.Length * sizeof(float)];
+                Buffer.BlockCopy(vectors, 0, byteArray, 0, byteArray.Length);
+
+                await CovenContext.Waembeddings.AddAsync(new Waembedding()
+                {
+                    EmbeddingId = Guid.NewGuid(),
+                    CharacterSetId = characterId,
+                    Vector = byteArray
+                });
+
+                await CovenContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
         #endregion
 
         #region Read
+        public async Task<User> GetUser(Guid userId)
+        {
+            try
+            {
+                return await CovenContext.Users
+                    .Include(x => x.WacharacterSets)
+                        .ThenInclude(x => x.Waembeddings)
+                    .FirstAsync(x => x.UserId == userId);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            try
+            {
+                return await CovenContext.Users.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<WacharacterSet>> GetUserEmbeddings(Guid userId)
+        {
+            try
+            {
+                return await CovenContext.WacharacterSets
+                    .Include(x => x.Waembeddings)
+                    .Where(x => x.UserId == userId)
+                    .ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
         #endregion
 
         #region Update
@@ -28,5 +129,17 @@ namespace Coven.Data.Repository
 
         #region Delete
         #endregion
+
+        public async Task<bool> SaveAsync()
+        {
+            try
+            {
+                return (await CovenContext.SaveChangesAsync() > 0);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
