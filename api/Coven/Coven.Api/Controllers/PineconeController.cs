3d9d7f256a99ec28169a2ae240ecae33a593ad35
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-05 15:30:00 -0600

    pinecone controller

---
+++ api/Coven/Coven.Api/Controllers/PineconeController.cs
@@ -0,0 +1,24 @@
+﻿using Coven.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coven.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PineconeController : ControllerBase
+    {
+        private readonly IPineconeService PineconeService;
+
+        public PineconeController(IPineconeService _pinecone)
+        {
+            PineconeService = _pinecone;
+        }
+
+        [HttpGet("GetIndexes")]
+        public async Task<ActionResult> GetIndexes()
+        {
+            return Ok(await PineconeService.QueryPineconeIndex());
+        }
+    }
+}
