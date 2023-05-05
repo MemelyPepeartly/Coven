61c2b4dae3b1e7a1f758e80008f01043bba7e9bf
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-04 18:22:53 -0600

    updated db scripts

--- planning/Create CovenDB.sql
+++ planning/Create CovenDB.sql
@@ -1,45 +1,29 @@
 CREATE SCHEMA app;
 GO
-CREATE TABLE app.Client
+CREATE TABLE app.[User]
 (
-    userId UNIQUEIDENTIFIER PRIMARY KEY ,
+    userId UNIQUEIDENTIFIER PRIMARY KEY,
     username NVARCHAR(50) NOT NULL,
-    email NVARCHAR(50) NOT NULL UNIQUE
+    email NVARCHAR(50) NOT NULL UNIQUE,
+    worldAnvilUsername NVARCHAR(50) NOT NULL UNIQUE
 );
-CREATE TABLE app.[Character]
+CREATE TABLE app.[WASnippet]
 (
-    characterId UNIQUEIDENTIFIER PRIMARY KEY,
-    characterName NVARCHAR(50) NOT NULL,
-    userId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.Client(userId) NOT NULL,
+    snippetId UNIQUEIDENTIFIER PRIMARY KEY,
+    userId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.[User](userId) NOT NULL,
+    worldAnvilArticleId UNIQUEIDENTIFIER NOT NULL,
+    title NVARCHAR(50) NOT NULL,
+    content NVARCHAR(MAX) NOT NULL
 );
-CREATE TABLE app.FeatureCategory
+CREATE TABLE app.[WACharacterSet]
 (
-    featureCategoryId UNIQUEIDENTIFIER PRIMARY KEY,
-    featureCategoryName NVARCHAR(25) NOT NULL,
-    featureCategoryDescription NVARCHAR(150) NOT NULL
+    characterSetId UNIQUEIDENTIFIER PRIMARY KEY,
+    userId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.[User](userId) NOT NULL,
+    characterSet NVARCHAR(MAX) NOT NULL
 );
-CREATE TABLE app.Feature
+CREATE TABLE app.[WAEmbedding]
 (
-    featureId UNIQUEIDENTIFIER PRIMARY KEY,
-    featureName NVARCHAR(25) NOT NULL,
-    featureCategoryId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.FeatureCategory(featureCategoryId) NOT NULL
-);
-CREATE TABLE app.CustomFeature
-(
-    customFeatureId UNIQUEIDENTIFIER PRIMARY KEY,
-    customFeatureName NVARCHAR(25) NOT NULL,
-    featureCategoryId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.FeatureCategory(featureCategoryId) NOT NULL
-);
-CREATE TABLE app.AttributeCategory
-(
-    attributeCategoryId UNIQUEIDENTIFIER PRIMARY KEY,
-    attributeCategoryName NVARCHAR(25) NOT NULL,
-    attributeCategoryDescription NVARCHAR(150) NOT NULL
-);
-CREATE TABLE app.Attribute
-(
-    attributeId UNIQUEIDENTIFIER PRIMARY KEY,
-    attributeName NVARCHAR(25) NOT NULL,
-    attributeValue NVARCHAR(35) NOT NULL,
-    attributeCategoryId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.AttributeCategory(attributeCategoryId) NOT NULL
+    embeddingId UNIQUEIDENTIFIER PRIMARY KEY,
+    characterSetId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.[WACharacterSet](characterSetId) NOT NULL,
+    vector VARBINARY(MAX) NOT NULL
 );
