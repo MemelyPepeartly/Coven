9f2a2e570908846a30899d0ebe37a508f72bf5b9
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-05 16:03:43 -0600

    unneeded code

--- api/Coven/Coven.Api/Controllers/AIController.cs
+++ api/Coven/Coven.Api/Controllers/AIController.cs
@@ -35,47 +35,6 @@ namespace Coven.Api.Controllers
             PineconeService = _pinecone;
         }
 
-        /// <summary>
-        /// Ideally used for sending in a query about a world or concept and getting back a list of worlds that are similar to the query.
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        [HttpPost("QueryWorlds")]
-        public async Task<ActionResult> QueryWorlds([FromBody]GetEmbeddingsRequestModel model)
-        {
-            EmbeddingsDTO DTO = new EmbeddingsDTO();
-            foreach (string word in model.userInput.Split(" "))
-            {
-                var thing = await _openAIAPI.Embeddings.CreateEmbeddingAsync(word);
-
-                DTO.queryEmbeddings.Add(new Embedding()
-                {
-                    characterSet = word,
-                    vectors = (await _openAIAPI.Embeddings.CreateEmbeddingAsync(word)).Data.SelectMany(v => v.Embedding).ToArray()
-                });
-            }
-
-            var worlds = await WorldAnvilService.GetWorlds();
-            List<string> articleTitles = (await WorldAnvilService.GetWorldArticlesSummary(worlds.worlds[1].id)).articles
-                .Select(s => s.title)
-                .ToList();
-
-            List<WACharacterSetDTO> existingEmbeddings = await Repository.GetUserEmbeddings(model.userId);
-
-            DTO.worldEmbeddings = existingEmbeddings.Select(e => new Embedding()
-            {
-                characterSet = e.characterSet,
-                vectors = e.vectors
-            }).ToList();
-
-            List<WACharacterSetDTO> result = GetRelatedEmbeddings(DTO).Select(e => new WACharacterSetDTO()
-            {
-                characterSet = e.characterSet,
-                //vectors = e.vectors
-            }).ToList();
-
-            return Ok(result);
-        }
 
         [HttpPost("AddEmbeddings")]
         public async Task<ActionResult> AddEmbeddings(Guid userId, Guid worldId)
@@ -110,57 +69,5 @@ namespace Coven.Api.Controllers
 
             return Ok();
         }
-
-        // Calculate the dot product of two vectors
-        private static float DotProduct(float[] vecA, float[] vecB)
-        {
-            float dotProduct = 0;
-            for (int i = 0; i < vecA.Length; i++)
-            {
-                dotProduct += vecA[i] * vecB[i];
-            }
-            return dotProduct;
-        }
-
-        // Calculate the magnitude of a vector
-        private static float Magnitude(float[] vec)
-        {
-            float sumOfSquares = 0;
-            for (int i = 0; i < vec.Length; i++)
-            {
-                sumOfSquares += vec[i] * vec[i];
-            }
-            return (float)Math.Sqrt(sumOfSquares);
-        }
-
-        // Calculate the cosine similarity between two vectors
-        private static float CalculateCosineSimilarity(float[] vecA, float[] vecB)
-        {
-            float dotProduct = DotProduct(vecA, vecB);
-            float magnitudeA = Magnitude(vecA);
-            float magnitudeB = Magnitude(vecB);
-            return dotProduct / (magnitudeA * magnitudeB);
-        }
-
-        // Get related embeddings using cosine similarity
-        public static List<Embedding> GetRelatedEmbeddings(EmbeddingsDTO embeddingsDTO, float similarityThreshold = 0.7f)
-        {
-            List<Embedding> relatedEmbeddings = new List<Embedding>();
-
-            foreach (Embedding queryEmbedding in embeddingsDTO.queryEmbeddings)
-            {
-                foreach (Embedding worldEmbedding in embeddingsDTO.worldEmbeddings)
-                {
-                    float similarity = CalculateCosineSimilarity(queryEmbedding.vectors, worldEmbedding.vectors);
-                    if (similarity >= similarityThreshold)
-                    {
-                        relatedEmbeddings.Add(worldEmbedding);
-                    }
-                }
-            }
-
-            return relatedEmbeddings;
-        }
-
     }
 }
