ccd97852b8ade9992e99c5e281933a704e00e10a
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-05 18:37:58 -0600

    json typing

--- api/Coven/Coven.Data/Pinecone/UpsertRequest.cs
+++ api/Coven/Coven.Data/Pinecone/UpsertRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -9,20 +10,21 @@ namespace Coven.Data.Pinecone
 {
     public class UpsertRequest
     {
+        [JsonProperty("vectors")]
         public List<Vector> Vectors { get; set; }
         /// <summary>
         /// The name of the associated dataset
         /// </summary>
+        [JsonProperty("namespace")]
         public string Namespace { get; set; }
     }
     public class Vector
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+        [JsonProperty("values")]
         public List<float> Values { get; set; }
-        public Metadata Metadata { get; set; }
-    }
-    public class Metadata
-    {
-        public string Genre { get; set; }
+        [JsonProperty("metadata")]
+        public PineconeMetadata Metadata { get; set; }
     }
 }
