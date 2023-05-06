2b8ebcdaea6639b606f09bf4f7f5a48268fec309
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-05 18:38:07 -0600

    metadata updates

---
+++ api/Coven/Coven.Data/Pinecone/PineconeMetadata.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coven.Data.Pinecone
+{
+    public abstract class PineconeMetadata { }
+
+    public class DefaultMetadata : PineconeMetadata 
+    {
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+    }
+
+    public class ArticleMetadata : PineconeMetadata
+    {
+        [JsonProperty("worldId")]
+        public string worldId { get; set; }
+        [JsonProperty("articleId")]
+        public string articleId { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("author")]
+        public string Author { get; set; }
+        [JsonProperty("articleType")]
+        public string ArticleType { get; set; }
+    }
+}
