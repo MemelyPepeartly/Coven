b1aad48aaf055188d8ae0216dea3e1932e424587
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-05 18:37:45 -0600

    new model

---
+++ api/Coven/Coven.Logic/Request Models/Post/CreateVectorsFromArticleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coven.Logic.Request_Models.Post
+{
+    public class CreateVectorsFromArticleModel
+    {
+        public Guid articleId { get; set; }
+    }
+}
