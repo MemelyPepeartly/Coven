df78782a9de9ab208822d7fb341a930f34e9b37b
MemelyPepeartly <memelypepeartly@gmail.com>
2023-05-17 14:39:23 -0600

    back to before threading

--- api/Coven/Coven.Api/Controllers/AIController.cs
+++ api/Coven/Coven.Api/Controllers/AIController.cs
@@ -51,29 +51,30 @@ namespace Coven.Api.Controllers
         {
             List<ArticleMeta> worldArticleMetaList = await WorldAnvilService.GetArticleMetas(worldId);
             WorldSegment worldSegment = await WorldAnvilService.GetWorld(worldId);
+            List<Embedding> embeddings = new List<Embedding>();
 
-            ConcurrentBag<Embedding> embeddingsBag = new ConcurrentBag<Embedding>();
-            ConcurrentBag<EmbedReport> articleReportBag = new ConcurrentBag<EmbedReport>();
+            List<Embedding> failedEmbeddings = new List<Embedding>();
+            List<EmbedReport> articleReport = new List<EmbedReport>();
 
-            // Run tasks in parallel to process articles
-            var articleProcessingTasks = worldArticleMetaList.Select(async meta =>
+            // Foreach article, get the embedding and add it to the list
+            foreach (ArticleMeta meta in worldArticleMetaList)
             {
                 Article article = await WorldAnvilService.GetArticle(meta.id);
                 if (string.IsNullOrEmpty(article.content))
                 {
-                    articleReportBag.Add(new EmbedReport()
+                    articleReport.Add(new EmbedReport()
                     {
                         success = false,
                         identifier = article.title,
                         message = "Article has no content"
                     });
-                    return;
+                    continue;
                 }
                 List<SentenceVectorDTO> articleVectors = await PineconeService.GetVectorsFromArticle(article);
 
                 foreach (SentenceVectorDTO entry in articleVectors)
                 {
-                    embeddingsBag.Add(new Embedding()
+                    embeddings.Add(new Embedding()
                     {
                         identifier = Guid.NewGuid().ToString(),
                         vectors = entry.Vector.ToArray(),
@@ -85,41 +86,23 @@ namespace Coven.Api.Controllers
                         }
                     });
                 }
-            });
-
-            try
-            {
-                await Task.WhenAll(articleProcessingTasks);
             }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-
-            
-
-            List<Embedding> ExceededSizeEmbeddings = embeddingsBag.Where(x => x.vectors.Length > 1536).ToList();
-            embeddingsBag = new ConcurrentBag<Embedding>(embeddingsBag.Where(x => !ExceededSizeEmbeddings.Contains(x)));
-            foreach (var e in ExceededSizeEmbeddings.Select(e => new EmbedReport()
+            List<Embedding> ExceededSizeEmbeddings = embeddings.Where(x => x.vectors.Length > 1536).ToList();
+            embeddings.RemoveAll(x => ExceededSizeEmbeddings.Contains(x));
+            articleReport.AddRange(ExceededSizeEmbeddings.Select(e => new EmbedReport()
             {
                 identifier = e.identifier,
                 success = false,
                 message = "Embedding size exceeded 1536"
-            }))
-            {
-                articleReportBag.Add(e);
-            }
+            }));
 
-            List<EmbedReport> articleReportList = articleReportBag.ToList();
-            List<Embedding> embeddingsList = embeddingsBag.ToList();
-
-            while (embeddingsList.Count > 0)
+            while (embeddings.Count > 0)
             {
                 // Pinecone has a limit of 250 vectors per request
-                int batchSize = Math.Min(250, embeddingsList.Count);
+                int batchSize = Math.Min(250, embeddings.Count);
 
                 // Create a separate list for the current batch
-                List<Embedding> batch = embeddingsList.Take(batchSize).ToList();
+                List<Embedding> batch = embeddings.Take(batchSize).ToList();
 
                 // Upload the batch
                 bool success = await PineconeService.UpsertVectors(worldSegment.name, batch);
@@ -127,7 +110,7 @@ namespace Coven.Api.Controllers
                 // Take any batches that failed and add them to the failed report
                 if (!success)
                 {
-                    articleReportList.AddRange(batch.Select(e => new EmbedReport()
+                    articleReport.AddRange(batch.Select(e => new EmbedReport()
                     {
                         identifier = e.identifier,
                         success = false,
@@ -140,7 +123,7 @@ namespace Coven.Api.Controllers
 #warning Could make a failsafe here to delete the entries if the database update fails
                     await Repository.CreatePineconeMetadataEntries(userId, batch);
 
-                    articleReportList.AddRange(batch.Select(e => new EmbedReport()
+                    articleReport.AddRange(batch.Select(e => new EmbedReport()
                     {
                         identifier = e.identifier,
                         success = true,
@@ -148,16 +131,16 @@ namespace Coven.Api.Controllers
                     }));
                 }
 
-                embeddingsList.RemoveRange(0, batchSize);
+                embeddings.RemoveRange(0, batchSize);
             }
 
             return Ok(new
             {
-                succeededOperationsCount = articleReportList.Where(x => x.success).Count(),
-                failedOperationsCount = articleReportList.Where(x => !x.success).Count(),
-                succeededOperations = articleReportList.Where(x => x.success),
-                failedOperations = articleReportList.Where(x => !x.success),
-                totalReport = articleReportList
+                succeededOperationsCount = articleReport.Where(x => x.success).Count(),
+                failedOperationsCount = articleReport.Where(x => !x.success).Count(),
+                succeededOperations = articleReport.Where(x => x.success),
+                failedOperations = articleReport.Where(x => !x.success),
+                totalReport = articleReport
             });
         }
     }
