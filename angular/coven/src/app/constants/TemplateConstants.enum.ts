07942e7420353270664ca39d19ec9e4aeb0fb117
MemelyPepeartly <memelypepeartly@gmail.com>
2023-11-09 23:33:51 -0700

    removed angular

--- angular/coven/src/app/constants/TemplateConstants.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum TemplateConstants {
-    person = "person",
-    document = "document",
-    settlement = "settlement",
-    report = "report"
-
-  }
--- angular/coven/src/app/interfaces/Author.ts
+++
@@ -1,5 +0,0 @@
-export interface Author {
-  id: string;
-  username: string;
-  url: string;
-}
