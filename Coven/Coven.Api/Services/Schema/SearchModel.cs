5cc9c4493668e1672978be5614e39198fb4272aa
MemelyPepeartly <memelypepeartly@gmail.com>
2023-11-11 03:13:50 -0700

    search model update

--- Coven/Coven.Api/Services/Schema/SearchModel.cs
+++ Coven/Coven.Api/Services/Schema/SearchModel.cs
@@ -1,33 +1,46 @@
-﻿using Azure.Search.Documents.Indexes.Models;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
 
 namespace Coven.Api.Services.Schema
 {
     public class SearchModel
     {
-        [SimpleField(IsKey = true, IsFilterable = true)]
+        [JsonPropertyName("@search.score")]
+        public double searchScore { get; set; }
+
+        [SimpleField(IsKey = true, IsFilterable = true, IsSortable = false)]
         public string worldContentId { get; set; }
 
-        [SearchableField(IsFilterable = true)]
+        [SearchableField(IsFilterable = true, IsSortable = true)]
         public string worldId { get; set; }
 
-        [SearchableField(IsFilterable = true)]
+        [SearchableField(IsFilterable = true, IsSortable = true)]
         public string articleId { get; set; }
 
-        [SearchableField(AnalyzerName = LexicalAnalyzerName.Values.EnLucene)]
+        [SearchableField(IsFilterable = true, IsSortable = true)]
+        public string worldAnvilArticleType { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true)]
+        public string author { get; set; }
+
+        [SearchableField(IsFilterable = false)]
         public string content { get; set; }
 
-        [SearchableField(IsFilterable = true, IsFacetable = true)]
+        [SearchableField(IsFilterable = true, IsSortable = true)]
+        public string articleTitle { get; set; }
+
+        [SearchableField(IsFilterable = true, IsFacetable = true, AnalyzerName = LexicalAnalyzerName.Values.StandardLucene)]
         public IList<string> people { get; set; }
 
-        [SearchableField(IsFilterable = true, IsFacetable = true)]
+        [SearchableField(IsFilterable = true, IsFacetable = true, AnalyzerName = LexicalAnalyzerName.Values.StandardLucene)]
         public IList<string> organizations { get; set; }
 
-        [SearchableField(IsFilterable = true, IsFacetable = true)]
+        [SearchableField(IsFilterable = true, IsFacetable = true, AnalyzerName = LexicalAnalyzerName.Values.StandardLucene)]
         public IList<string> locations { get; set; }
 
-        [SearchableField(IsFilterable = true, IsFacetable = true)]
+        [SearchableField(IsFilterable = true, IsFacetable = true, AnalyzerName = LexicalAnalyzerName.Values.StandardLucene)]
         public IList<string> keyPhrases { get; set; }
     }
-
 }
