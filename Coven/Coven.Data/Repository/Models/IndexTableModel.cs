def504a8da9601a4380c20c5201709d0b681f66d
MemelyPepeartly <memelypepeartly@gmail.com>
2023-11-11 03:14:54 -0700

    index model

---
+++ Coven/Coven.Data/Repository/Models/IndexTableModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coven.Data.Repository.Models
+{
+    public class IndexTableModel
+    {
+        public string content { get; set; }
+        public string articleTitle { get; set; }
+        public string worldAnvilArticleType { get; set; }
+        public string author { get; set; }
+        public Guid articleId { get; set; }
+        public Guid worldId { get; set; }
+    }
+}
